2e849f90b228ed1e314a3cebba1099aa6653cb6a
Akash Singh <2081993akash@gmail.com>
2026-06-01 10:01:19 +0530

    routing and methods

--- src/app.js
+++ src/app.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send(req.query);
+  // res.send("User Get method");
+});
+
+app.get("/dynamicUser/:id/:name/:age", (req, res) => {
+  res.send(req.params);
+  // res.send("Dynamic User Get method");
+});
+
+app.post("/user", (req, res) => {
+  res.send("User Post method");
+});
+
+app.put("/user", (req, res) => {
+  res.send("User Put method");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User Patch method");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User Delete method");
+});
+
 app.use("/hello", (req, res) => {
   res.send("Hello to the World!");
 });
