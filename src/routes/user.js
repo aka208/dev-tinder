2c282b21ac594836664fba41f38130b28b1c555e
Akash Singh <2081993akash@gmail.com>
2026-06-18 14:43:36 +0530

    Feed API changes

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,8 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/userAuth");
 const connectionRequest = require("../models/connectionRequest");
-
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const USER_SAFE_DATA = "firstName lastName photoUrl skills age gender";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -49,6 +50,39 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
 
     res.json({ message: "Connections fetched", data: userData });
-  } catch (error) {}
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const page = req.query.page;
+    let limit = req.query.limit;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    const loggedInUserId = req.user._id;
+    const connections = await connectionRequest
+      .find({
+        $or: [{ fromUserId: loggedInUserId }, { toUserId: loggedInUserId }],
+      })
+      .select("fromUserId toUserId");
+    const hideUsers = new Set();
+    connections.forEach((c) => {
+      hideUsers.add(c);
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsers) } },
+        { _id: { $ne: loggedInUserId } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ message: "User Feeds", data: users });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
 });
 module.exports = userRouter;
